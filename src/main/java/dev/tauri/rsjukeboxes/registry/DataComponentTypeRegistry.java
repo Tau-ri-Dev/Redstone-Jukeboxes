db9bb3dab831e7961e884a5cc22bd51963e9a9d1
MineDragonCZ_ <minedragoncz@email.cz>
2025-01-14 19:28:27 +0100

    Idk

---
+++ src/main/java/dev/tauri/rsjukeboxes/registry/DataComponentTypeRegistry.java
@@ -0,0 +1,9 @@
+package dev.tauri.rsjukeboxes.registry;
+
+import net.minecraft.core.component.DataComponentType;
+import net.minecraft.core.registries.Registries;
+import net.minecraftforge.registries.DeferredRegister;
+
+public class DataComponentTypeRegistry {
+    public static final DeferredRegister<DataComponentType<?>> REGISTER = DeferredRegister.create(Registries.DATA_COMPONENT_TYPE, )
+}
